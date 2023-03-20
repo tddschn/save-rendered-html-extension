3dffca4c465192f2c598d1c03e4736ba999ef1e6
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-03-20 19:05:14 +0800

    2023-03-20T19-05-14Z

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getHtml") {
     const renderedHtml = document.documentElement.outerHTML;
-    const fileName = `rendered_html_${new Date().toISOString()}.html`;
+    const fileName = `rendered_html_${new Date().toISOString().replace(/[:.]/g, "-")}.html`;
 
     const blob = new Blob([renderedHtml], { type: "text/html" });
     const url = URL.createObjectURL(blob);
@@ -9,4 +9,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.runtime.sendMessage({ action: "saveHtml", url, fileName });
   }
 });
-
