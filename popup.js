39aa4cfb023d4f2422b09a2118dfc4e5272ce34d
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-03-20 19:17:52 +0800

    2023-03-20T19-17-52Z

--- popup.js
+++ popup.js
@@ -1,19 +1,33 @@
 document.getElementById("saveHtmlBtn").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.tabs.executeScript(tab.id, { code: 'document.documentElement.outerHTML;' }, (results) => {
-    const renderedHtml = results[0];
+  try {
+    const result = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: getRenderedHtml,
+    });
+
+    const renderedHtml = result[0].result;
     const fileName = `rendered_html_${new Date().toISOString().replace(/[:.]/g, "-")}.html`;
 
     const blob = new Blob([renderedHtml], { type: "text/html" });
     const url = URL.createObjectURL(blob);
 
-    chrome.downloads.download({
-      url: url,
-      filename: fileName,
-      saveAs: true,
-    }, () => {
-      URL.revokeObjectURL(url);
-    });
-  });
+    chrome.downloads.download(
+      {
+        url: url,
+        filename: fileName,
+        saveAs: true,
+      },
+      () => {
+        URL.revokeObjectURL(url);
+      }
+    );
+  } catch (error) {
+    console.error("Error executing script:", error);
+  }
 });
+
+function getRenderedHtml() {
+  return document.documentElement.outerHTML;
+}
