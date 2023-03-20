4053766d5850b0f25f41e58451466cf6b86a0f7e
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-03-20 19:05:39 +0800

    2023-03-20T19-05-39Z

--- content.js
+++ content.js
@@ -3,9 +3,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const renderedHtml = document.documentElement.outerHTML;
     const fileName = `rendered_html_${new Date().toISOString().replace(/[:.]/g, "-")}.html`;
 
-    const blob = new Blob([renderedHtml], { type: "text/html" });
-    const url = URL.createObjectURL(blob);
-
-    chrome.runtime.sendMessage({ action: "saveHtml", url, fileName });
-  }
-});
+    const blob = new Blob([renderedHtml], { type: "
--- popup.js
+++ popup.js
@@ -2,4 +2,3 @@ document.getElementById("saveHtmlBtn").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.tabs.sendMessage(tab.id, { action: "getHtml" });
 });
-
