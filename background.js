6f2dbbe5c2b8bee2efd5525874adff508969ebd8
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-11-10 22:38:24 -0500

    2023-11-10T22-38-24Z

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(handleMessage);
 
 async function handleMessage(message, sender, sendResponse) {
-  if (message.action === 'saveHtml') {
+  if (message.action === "saveHtml") {
     try {
       await chrome.downloads.download({
         url: message.url,
@@ -9,9 +9,26 @@ async function handleMessage(message, sender, sendResponse) {
         saveAs: true,
       });
     } catch (error) {
-      console.error('Error downloading file:', error);
+      console.error("Error downloading file:", error);
     }
   }
 }
+
+// Register context menu
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "save-html-without-scripts",
+    title: "Save HTML without <script> tags",
+    contexts: ["all"],
+  });
+});
+
+// Context Menu onClicked event
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "save-html-without-scripts") {
+    // The existing handleMessage function could be reused or modified as needed.
+    chrome.tabs.sendMessage(tab.id, { action: "getHtmlWithoutScripts" });
+  }
+});
 
 export { handleMessage };
--- content.js
+++ content.js
@@ -1,26 +1,48 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getHtml") {
-    const renderedHtml = document.documentElement.outerHTML;
-    const title = document.title;
-    const url = window.location.href;
-    const sanitizedTitle = title.replace(/[\\/:*?"<>|]/g, "_");
-    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-    const fileName = `rendered_html_${timestamp}_${sanitizedTitle}__${btoa(
-      url
-    )}.html`;
-
-    const blob = new Blob([renderedHtml], { type: "text/html" });
-    const urlObj = URL.createObjectURL(blob);
-
-    chrome.downloads.download(
-      {
-        url: urlObj,
-        filename: fileName,
-        saveAs: true,
-      },
-      () => {
-        URL.revokeObjectURL(urlObj);
-      }
-    );
+    // This block remains the same as before
+    sendHtmlResponse();
+  } else if (message.action === "getHtmlWithoutScripts") {
+    // This is the new block for removing <script> tags
+    sendHtmlResponse(true);
   }
 });
+
+function sendHtmlResponse(removeScripts = false) {
+  let renderedHtml = document.documentElement.outerHTML;
+  let title = document.title;
+  let url = window.location.href;
+
+  if (removeScripts) {
+    // Create a new DOM parser and parse the rendered HTML
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(renderedHtml, "text/html");
+
+    // Remove all script tags from the parsed document
+    const scripts = doc.querySelectorAll("script");
+    scripts.forEach((script) => script.remove());
+
+    // Serialize the document back to HTML
+    renderedHtml = doc.documentElement.outerHTML;
+  }
+
+  const sanitizedTitle = title.replace(/[\\/:*?"<>|]/g, "_");
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  const fileName = `rendered_html_${timestamp}_${sanitizedTitle}__${btoa(
+    url
+  )}.html`;
+
+  const blob = new Blob([renderedHtml], { type: "text/html" });
+  const urlObj = URL.createObjectURL(blob);
+
+  chrome.downloads.download(
+    {
+      url: urlObj,
+      filename: fileName,
+      saveAs: true,
+    },
+    () => {
+      URL.revokeObjectURL(urlObj);
+    }
+  );
+}
